c5167fe15c2a8eb13485db416282a6f1e7c7f1fa
aefmoscow <aefmoscow@gmail.com>
2024-11-15 21:46:03 +0300

    2

---
+++ модуль2/2.3.py
@@ -0,0 +1,15 @@
+# проверочная строка на выход диапазона
+# my_list = [42, 69, 322, 13, 0, 99, 5, 9, 8, 7, 6, 5]
+# начало прорграммы
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+inn = 0
+l = len(my_list) - 1
+# print('Eto L', l)
+while my_list[inn] > 0:
+    print(my_list[inn])
+    inn = inn + 1
+    if my_list[inn] == 0:
+        inn = inn + 1
+    elif inn == l:
+        break
+    continue
