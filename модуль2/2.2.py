4a809de655e5aafdcc4050296256d8b039df005d
aefmoscow <aefmoscow@gmail.com>
2024-11-15 20:57:54 +0300

    0

---
+++ модуль2/2.2.py
@@ -0,0 +1,12 @@
+first = input('Input first ')
+second = input('Input second ')
+Third = input('Input Third ')
+# print(first, second, Third)
+if first == second == Third :
+        print ('Vse ravni,', 3)
+elif first == second or  second == Third  or first ==  Third :
+    print('Poparno  ravni,', 2)
+#можно и так
+# elif first != second and  second != Third  and first !=  Third :
+else:
+    print('Ne  ravni,', 0)
