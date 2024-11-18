9338bd0b56468a1db28ea74d1926b1e9a2700e1c
aefmoscow <aefmoscow@gmail.com>
2024-11-18 11:39:22 +0300

    2

---
+++ модуль2/2.4.py
@@ -0,0 +1,8 @@
+# дан список
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# создать пустые списки  primes и not_primes
+primes = []
+not_primes = []
+# При помощи цикла for переберите список numbers.
+for i in range(0, len(numbers)):
+    print(numbers[i])
