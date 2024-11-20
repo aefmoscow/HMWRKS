84106c118763583a468c3c8b3f51d65dbe2a13b1
aefmoscow <151546020+aefmoscow@users.noreply.github.com>
2024-11-20 09:22:13 +0300

    Update 2.4.py

--- модуль2/2.4.py
+++ модуль2/2.4.py
@@ -6,10 +6,20 @@ not_primes = []
 # При помощи цикла for переберите список numbers.
 for i in range(0, len(numbers)):
     print(numbers[i])
-    primes = numbers[i]
-    not_primes =  numbers[i]
+#условия что число простое:
+def is_prime(numbers[i]):
+    if numbers[i] <= 1:
+        return False
+    for j in range(2, int(numbers[i]**0.5) + 1):
+        if n % j == 0:
+            not_primes = numbers[i]
+            return False
+    primes[i] = numbers[i]
+    return True
+    
+    
 
-    is_prime=[True]
+  #  is_prime=[True]
 
 
 print('primes', primes)
