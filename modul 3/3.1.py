e38b33bac32ad2ae230a9d92dc39ec2813489180
aefmoscow <aefmoscow@gmail.com>
2024-11-22 19:03:15 +0300

    2

---
+++ modul 3/3.1.py
@@ -0,0 +1,7 @@
+calls = 0
+def count_calls
+    calls =
+
+def string_info(string)
+
+def is_contains(string, list_to_search)
--- модуль2/2.6.py
+++ модуль2/2.6.py
@@ -1,9 +1,9 @@
 # комментарии не читайте!!! все для обучения на pythontutor.com
 
-for lft_stn in range(3, 21): #(от 3 до 20) случайным образом
-    rght_stn = f' Pass for [{lft_stn}]' #подготовка приставки в цикле
+for lft_stn in range(3, 21):  # (от 3 до 20) случайным образом
+    rght_stn = f' Pass for [{lft_stn}]'  # подготовка приставки в цикле
     for a in range(1, lft_stn):
         for b in range(2, lft_stn):
-            if lft_stn % (a + b) == 0: #если нет- идем пересчитывать в 6 стр
-                rght_stn =  f'{rght_stn}:  {a} {b} '
+            if lft_stn % (a + b) == 0:  # если нет- идем пересчитывать в 6 стр
+                rght_stn = f'{rght_stn}:  {a} {b} '
     print(rght_stn)
