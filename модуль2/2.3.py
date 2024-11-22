c3596dcbc49f3c1e5dd83cc0f6f7417405a567f1
aefmoscow <aefmoscow@gmail.com>
2024-11-22 12:03:57 +0300

    2

--- модуль2/2.3.py
+++ модуль2/2.3.py
@@ -2,10 +2,9 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 # назначаем переменную номера элемента
 el_ind = 0
-itemlist=[]
 while el_ind < len(my_list):
     item = my_list[el_ind]
-    el_ind = el_ind + 1
+    el_ind +=  1
     if item == 0:
         continue
     elif item < 0:
