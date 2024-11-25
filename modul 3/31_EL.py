fda374c5757e0288fb61dfb9081facaa27d9f1e8
aefmoscow <aefmoscow@gmail.com>
2024-11-25 13:31:48 +0300

    2

--- modul 3/31_EL.py
+++ modul 3/31_EL.py
@@ -15,3 +15,9 @@ send_email('Это сообщение для проверки связи', 'vasy
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+'''
+Вывод на консоль:
+Письмо успешно отправлено с адреса university.help@gmail.com на адрес vasyok1337@gmail.com
+НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса urban.info@gmail.com на адрес urban.fan@mail.ru
+Невозможно отправить письмо с адреса urban.teacher@mail.uk на адрес urban.student@mail.ru
+Нельзя отправить письмо самому себе!'''
