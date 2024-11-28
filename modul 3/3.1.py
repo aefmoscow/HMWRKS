0a7e9de77318425ee88cc1fe7ea28877cc72f6f1
aefmoscow <aefmoscow@gmail.com>
2024-11-28 20:01:12 +0300

    2

--- modul 3/3.1.py
+++ modul 3/3.1.py
@@ -1,19 +1,21 @@
-
 def count_calls():
     global calls
     calls += 1
+
 
 def string_info(string):
     count_calls()
     return (len(string), string.upper(), string.lower())
 
+
 def is_contains(string, list_to_search):
     count_calls()
     lowercase_list = [elem.lower() for elem in list_to_search]
-    if string.lower() in lowercase_list :
+    if string.lower() in lowercase_list:
         return True
     else:
         return False
+
 
 calls = 0
 print(f'Для наглядности {type(string_info("Capybara"))}')
@@ -22,5 +24,3 @@ print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
 print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
 print(calls)
-# lowercase_list = [s.lower() for s in list_to_search]
-# print(list_to_search)
