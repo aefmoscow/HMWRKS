f59947edcf7a3151977eb53ebc5f0aecf94464df
aefmoscow <aefmoscow@gmail.com>
2024-11-28 20:00:24 +0300

    2

--- modul 3/3.1.py
+++ modul 3/3.1.py
@@ -1,31 +1,26 @@
-# комментарии не читайте!!! все для обучения на pythontutor.com
-calls = 0  # Создать переменную calls = 0 вне функций.
-# подсчитывающая вызовы остальных функций. Создать функцию count_calls и изменять в ней значение переменной calls.
+
 def count_calls():
     global calls
     calls += 1
-# приведем к нижнему индексу
-def lowing_string(elem):
-    a = elem.lower()
-    return a
-# принимает аргумент - строку и возвращает кортеж из: длины этой строки, строку в верхнем регистре, строку в нижнем регистре.
-# Создать функцию string_info с параметром string
+
 def string_info(string):
     count_calls()
-    return f'({len(string)}, {string.upper()}, {string.lower()})'
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list_to_search):
+    count_calls()
+    lowercase_list = [elem.lower() for elem in list_to_search]
+    if string.lower() in lowercase_list :
+        return True
+    else:
+        return False
+
+calls = 0
+print(f'Для наглядности {type(string_info("Capybara"))}')
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
-# Создать функцию is_contains с двумя параметрами string и list_to_search, реализовать логику работы по описанию.
-# принимает два аргумента: строку и список, и возвращает True, если строка находится в этом списке, False - если отсутствует. Регистром строки при проверке пренебречь:
-def is_contains(string, list_to_search):
-    for i in range(1, len(list_to_search)):
-        if lowing_string(string) in lowing_string(list_to_search[i]):
-            count_calls()
-            return True
-    else:
-        count_calls()
-        return False
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
 print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
-
 print(calls)
+# lowercase_list = [s.lower() for s in list_to_search]
+# print(list_to_search)
--- модуль2/test.py
+++ модуль2/test.py
@@ -1,19 +1,80 @@
-# from random import choice
-#
-# my_list = [1, 2, 3, 4, 5]
-#
-# for i in range(5):
-#     print(choice(my_list))
-#
-# def lottery():
-#     tickets = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-#     win = len(tickets)
-#     return win
-# win1 = lottery() + lottery()
-# print(win1)
-nums =(4,'yuyu',67)
-for i in range(len(nums)):
+calls = 0 # Все эти значения должны быть в общем месте, для того,
 
-    print(i)
+# Чтобы можно было быстрее считывать работу вашего кода.
 
-print(nums)
+def count_calls():
+    global calls
+    calls += 1
+
+def lower_string(elem): #Бесполезная функция, вы можете сделать преобразование в основной функции,
+
+# А так вы просто забрали у себя время на написание данной конструкции.
+
+    a = elem.lower()
+
+return a
+
+def string_info(string):
+    count_calls()
+    return f'({len(string)}, {string.upper()}, {string.lower()})' # F- строка вам тут не нужна, вам нужно вернуть кортеж
+# А вы возвращаете строку
+
+print(f'Для наглядности {type(string_info("Capybara"))}') # Все эти значения должны быть в общем месте, для того,
+
+# Чтобы можно было быстрее считывать работу вашего кода.
+
+print(string_info('Capybara')) # Все эти значения должны быть в общем месте, для того,
+
+# Чтобы можно было быстрее считывать работу вашего кода.
+
+print(string_info('Armageddon')) # Все эти значения должны быть в общем месте, для того,
+
+# Чтобы можно было быстрее считывать работу вашего кода.
+
+def is_contains(string, list_to_search):
+
+    for i in range(1, len(list_to_search)): # Для чего перебирать по индексам, если можно перебирать сразу элементы?
+
+# Если перебирать элементы, то переменную i нужно переименовать в elem
+
+list_to_search_l = lower_string(list_to_search[i]) #Тогда вы сможете избавиться от этой строки
+
+if lower_string(string) in list_to_search_l: # Ну а далее уже нужно будет просто подредактировать код
+
+count_calls() #Для чего нам 2 раза писать одинаковую строку кода, если мы можем засунуть её в самое начало
+
+# Вашей функции is_contains - 1 раз
+
+return True
+
+else:
+
+count_calls() #Для чего нам 2 раза писать одинаковую строку кода, если мы можем засунуть её в самое начало
+
+# Вашей функции is_contains - 1 раз
+
+return False
+
+
+
+# ВОТ В ЭТО МЕСТО МЫ ВСЁ ПЕРЕНОСИМ, ЧТОБЫ МОЖНО БЫЛО ОТСЛЕДИТЬ ПОРЯДОК ВЫПОЛНЕНИЯ ВСЕХ ФУНКЦИЙ И ЗНАЧЕНИЙ
+
+# КОТОРЫЕ ТУДА ПЕРЕДАЮТСЯ
+
+calls = 0
+
+print(f'Для наглядности {type(string_info("Capybara"))}')
+
+print(string_info('Capybara'))
+
+print(string_info('Armageddon'))
+
+
+
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+
+
+
+print(calls)
