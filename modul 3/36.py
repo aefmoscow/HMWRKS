475e42b8c74e58095432de0e3f2180a253739289
aefmoscow <aefmoscow@gmail.com>
2024-12-02 07:44:01 +0300

    2

--- modul 3/36.py
+++ modul 3/36.py
@@ -1,20 +1,32 @@
+# Исходный объект типа список
 data_structure = [
-  [1, 2, 3],
-  {'a': 4, 'b': 5},
-  (6, {'cube': 7, 'drum': 8}),
-  "Hello",
-  ((), [{(2, 'Urban', ('Urban2', 35))}])
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
-for i in range(len(data_structure)):
-  print('Elem  ', i, type(data_structure[i]), data_structure[i])
-# 1 + 2 + 3 + len('a') + 4 + len('b') + 5 + 6 + len('cube') + 7 + .... + 35 = 99
 
-def calculate_structure_sum(data_structure):
-  global  summ
-  for i in range(len(data_structure)):
-    if isinstance(data_structure[i], str):
+def calculate_structure_sum(dat):
+    global summa
+    for el in dat:
+        if isinstance(el, dict):
+            calculate_structure_sum(el.items())
+        if isinstance(el, (list, tuple, set)):
+            calculate_structure_sum(el)
+        if isinstance(el, str):
+            summa += len(el)
+        elif isinstance(el, (int, float)):
+            summa += el
+    return summa
 
-summ = 0
-# result = calculate_structure_sum(data_structure)
 
-# print(result)
+summa = 0
+result = calculate_structure_sum(data_structure)
+print(result)
+
+# Проверялка понятная (Вопрос: почему №2неверно выдает сумму построчно???Пусть бы по нарастающей, но криво считает)
+# for i in range(len(data_structure)):
+#     print('Elem  ', i, type(data_structure[i]), data_structure[i])
+    #2 print('Elem  ', i, type(data_structure[i]), data_structure[i], 'Сумма ', calculate_structure_sum(data_structure[i]))
+
