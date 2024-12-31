bd37ef150b5e5d20ad95a8ab1e3a2805f3217716
Aefmoscow <Aefmoscow@gmail.com>
2024-12-31 18:12:38 +0300

    1

--- module5/5.5.py
+++ module5/5.5.py
@@ -1,1 +1,57 @@
-# Hi
+class User:
+    def __init__(self, nickname, password, age):
+        self.nickname= nickname
+        self.password = password
+        self.age = age
+
+
+class Video:
+    def __init__(self, title, duration, time_now = 0, adult_mode = bool(False)):
+        self.title = title
+        self.duration = duration
+        self.time_now = time_now
+        self.adult_mode = adult_mode
+
+    def __str__(self):
+    def __eq__(self, other):
+    def __contains__(self, item):
+
+class UrTube:
+    def __init__(self, users, videos, current_user):
+        self.users = users
+        self.videos = videos
+        self.current_user = current_user
+
+    def log_in(self, nickname, password):
+        if nickname == user.nickname
+
+    def register(self, nickname, password, age):
+        if nickname == user.nickname:
+            print(f'Пользователь {nickname} уже существует')
+    def add(self, ):
+
+
+    def get_videos(self, ):
+    def watch_video(self, ):
+
+
+# Код для проверки:
+ur = UrTube()
+v1 = Video('Лучший язык программирования 2024 года', 200)
+v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+# Добавление видео
+ur.add(v1, v2)
+# Проверка поиска
+print(ur.get_videos('лучший'))
+print(ur.get_videos('ПРОГ'))
+# Проверка на вход пользователя и возрастное ограничение
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+ur.watch_video('Для чего девушкам парень программист?')
+# Проверка входа в другой аккаунт
+ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+print(ur.current_user)
+# Попытка воспроизведения несуществующего видео
+ur.watch_video('Лучший язык программирования 2024 года!')
