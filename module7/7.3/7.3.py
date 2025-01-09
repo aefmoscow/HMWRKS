35169b6b7c9391a2b5b97445e04627f41bbbcd5a
aefmoscow <aefmoscow@gmail.com>
2025-01-09 21:30:34 +0300

    1

--- module7/7.3/7.3.py
+++ module7/7.3/7.3.py
@@ -1,16 +1,39 @@
 class WordsFinder:
-    def __init__(self):
-        self.file_names = ()
-    def get_all_words(self, file_names):
-        all_words = {}
-        with open(self.file_names) as file:
-            for inwrite_string in file_names:
-            all_words[(strings_positions, byte)] = inwrite_string
+    def __init__(self, file_names):
+        self.file_names = file_names
 
-        return
+    def get_all_words(self):
+        all_words = {}
+        with open(str(self.file_names), 'r', encoding='utf-8') as file:
+            words = file.read().lower()
+            for punct in [',', '.', '=', '!', '?', ';', ':', ' - ']:
+                words = words.replace(punct, '')
+                all_words[self.file_names] = words.split()
+                return all_words
+
+    def count(self, word):
+        word = word.lower()
+        word_counts = {}
+        for (file_name_co, word_co) in self.get_all_words().items():
+            word_counts[file_name_co] = word_co.count(word)
+        return word_counts
+
+    def find(self, word):
+        word = word.lower()
+        word_at = {}
+        for (file_name_fi, word_fi) in self.get_all_words().items():
+            if word in word_fi:
+                word_at[file_name_fi] = word_fi.index(word) + 1
+        return word_at
+
 
 # Пример выполнения программы:
-finder2 = WordsFinder('73test_file.txt')
+finder2 = WordsFinder('c:/USERS/DV/desktop/p/73test_file.txt')
 print(finder2.get_all_words())  # Все слова
 print(finder2.find('TEXT'))  # 3 слово по счёту
 print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
+
+finder1 = WordsFinder('Mother Goose - Monday’s Child.txt', )
+print(finder1.get_all_words())
+print(finder1.find('Child'))
+print(finder1.count('Child'))
